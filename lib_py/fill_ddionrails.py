25ac7738dc6bb967a7bf0df90a107564d535557a
Marcel Hebing <marcel@ddionrails.org>
2016-11-07 13:23:33 +0100

    Generate link to question from variable name for p and h datasets

--- lib_py/fill_ddionrails.py
+++ lib_py/fill_ddionrails.py
@@ -1,7 +1,10 @@
-import os, sys
+import os, sys, re
 import pandas as pd
 
 sys.path.append(os.path.expanduser("~/github/ddi.py/"))
+
+Q_IN_V_RE = re.compile(r'^[a-z]*([0-9]{2})')
+I_IN_V_RE = re.compile(r'^([a-z]{1,2})([hp])')
 
 from ddi.onrails.repos import merge_instruments, dor1, copy, convert_r2ddi
 
@@ -17,7 +20,7 @@ def datasets():
     dor1.lower_all_names(x)
     x.to_csv("ddionrails/datasets.csv", index=False)
 
-def variables():
+def read_variables():
     x = pd.read_csv("metadata/variables.csv")
     x.rename(columns={
         "study":"study_name",
@@ -28,7 +31,38 @@ def variables():
     valid = x.ix[ : , ("study_name", "dataset_name", "variable_name")].duplicated() == False
     x = x.ix[valid]
     dor1.lower_all_names(x)
+    return x
+
+def variables():
+    x = read_variables()
     x.to_csv("ddionrails/variables.csv", index=False)
+
+def questions_variables():
+    x = read_variables()
+    x = x[["study_name", "dataset_name", "variable_name"]]
+    def determin_question(x):
+        try:
+            return Q_IN_V_RE.match(x).group(1)
+        except:
+            return None
+    x["question_name"] = x["variable_name"].apply(determin_question)
+    def determin_instrument(x):
+        year_letters = list("abcdefghijklmnopqrstuvwxyz")
+        year_letters += [ "b%s" % x for x in list("abcdefghijklm") ]
+        try:
+            year_letter = I_IN_V_RE.match(x).group(1)
+            p_or_h = I_IN_V_RE.match(x).group(2)
+            if p_or_h == "p":
+                instrument_type = "pe"
+            else:
+                instrument_type = "hh"
+            year = year_letters.index(year_letter) + 1984
+            return "soep-core-%s-%s" % (year, instrument_type)
+        except:
+            return None
+    x["instrument_name"] = x["variable_name"].apply(determin_instrument)
+    x.dropna(inplace=True)
+    x.to_csv("ddionrails/questions_variables.csv", index=False)
 
 def concepts():
     x = pd.read_csv("metadata/concepts.csv")
