4115d8609cd2726667c635214b93985877efbb08
mhebing <marcel@ddionrails.org>
2017-11-23 07:40:44 +0100

    Update fill_ddionrails to v33

--- lib_py/convert_datasets.py
+++ lib_py/convert_datasets.py
@@ -7,7 +7,7 @@ from ddi.onrails.repos import convert_r2ddi
 
 def main():
     print("[INFO] Read datasets...")
-    data = convert_r2ddi.Parser("soep-core", version="v32")
+    data = convert_r2ddi.Parser("soep-core", version="v33")
     print("[INFO] Write JSON file...")
     data.write_json()
     print("[INFO] Write YAML file...")
