caa69de3384afe2e5c4bdef5c90bf14e6f1f4543
afuetterer <fuetterh@posteo.de>
2019-04-05 11:16:57 +0200

    Remove function questions_variables() from fill_ddionrails.py (#48)

--- lib_py/fill_ddionrails.py
+++ lib_py/fill_ddionrails.py
@@ -1,12 +1,14 @@
 import re
 
 import pandas as pd
-from concepts_questions import create_concepts_questions
 from ddi.onrails.repos import convert_r2ddi, copy, dor1, merge_instruments
 from ddi.onrails.repos.topics import TopicParser
 
-Q_IN_V_RE = re.compile(r"^[a-z]*([0-9]{2})")
-I_IN_V_RE = re.compile(r"^([a-z]{1,2})([hp])")
+from concepts_questions import create_concepts_questions
+from questions_variables import questions_from_generations
+
+STUDY = "soep-core"
+VERSION = "v34"
 
 
 def datasets():
@@ -49,50 +51,10 @@ def variables():
     x.to_csv("ddionrails/variables.csv", index=False)
 
 
-def questions_variables():
-    x = read_variables()
-    x = x[["study_name", "dataset_name", "variable_name"]]
-
-    def determin_question(x):
-        try:
-            i = Q_IN_V_RE.match(x).group(1)
-            if i[0] == "0":
-                return i[1]
-            else:
-                return i
-        except:
-            return None
-
-    x["question_name"] = x["variable_name"].apply(determin_question)
-
-    def determin_instrument(x):
-        year_letters = list("abcdefghijklmnopqrstuvwxyz")
-        year_letters += ["b%s" % x for x in list("abcdefghijklm")]
-        try:
-            year_letter = I_IN_V_RE.match(x).group(1)
-            p_or_h = I_IN_V_RE.match(x).group(2)
-            if p_or_h == "p":
-                instrument_type = "pe"
-            else:
-                instrument_type = "hh"
-            year = year_letters.index(year_letter) + 1984
-            return "soep-core-%s-%s" % (year, instrument_type)
-        except:
-            return None
-
-    x["instrument_name"] = x["variable_name"].apply(determin_instrument)
-    x.dropna(inplace=True)
-    x.to_csv("ddionrails/questions_variables.csv", index=False)
-
-
 def concepts():
     x = pd.read_csv("metadata/concepts.csv")
     x.rename(
-        columns={
-            # "concept":"concept_name",
-            "topic_prefix": "topic_name"
-        },
-        inplace=True,
+        columns={"concept": "concept_name", "topic_prefix": "topic_name"}, inplace=True
     )
     valid = x.ix[:, "concept_name"].duplicated() == False
     x = x.ix[valid]
@@ -105,8 +67,8 @@ def main():
     concepts()
     datasets()
     variables()
-    questions_variables()
-    convert_r2ddi.Parser("soep-core", version="v33").write_json()
+    questions_from_generations(VERSION)
+    convert_r2ddi.Parser(STUDY, version=VERSION).write_json()
     merge_instruments.main()
     copy.f("publications.csv")
     copy.f("topics.csv")
